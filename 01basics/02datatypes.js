5992cf6430f49aa55e81eaf4d1e8861e61782313
Gurmeet13Shimar <shimargunjan@gmail.com>
2025-03-27 08:17:05 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("gunjan") we are using nodejs not browser
+
+/*
+1.number
+2.bigint
+3.string
+4.boolean
+5.null
+6.undefined
+7.symbol:unique or not unique
+*/ 
+/*
+object
+ */
+
+// typeof null is object
